e1ab9d2079cb8aa50230f4212259e4fddfc93d4f
Kunal Gautam <kgautam@lxplus724.cern.ch>
2021-12-03 01:03:38 +0100

    ntuplers

--- examples/FCCee/KG/analysis_MC.py
+++ examples/FCCee/KG/analysis_MC.py
@@ -31,54 +31,60 @@ class analysis():
         df2 = (self.df
 
                .Alias("Particle0", "Particle#0.index")
+               .Alias("Particle1", "Particle#1.index")
                .Alias("Jet3", "Jet#3.index") #alias for dealing with # in python
 
-               .Define("RP_px",  "ReconstructedParticle::get_px(ReconstructedParticles)")
-               .Define("RP_py",  "ReconstructedParticle::get_py(ReconstructedParticles)")
-               .Define("RP_pz",  "ReconstructedParticle::get_pz(ReconstructedParticles)")
-               .Define("RP_p",  "ReconstructedParticle::get_p(ReconstructedParticles)")
-               .Define("RP_e",  "ReconstructedParticle::get_e(ReconstructedParticles)")
-               .Define("RP_theta",  "ReconstructedParticle::get_theta(ReconstructedParticles)")
-               .Define("RP_mass",  "ReconstructedParticle::get_mass(ReconstructedParticles)")
-               .Define("RP_charge",  "ReconstructedParticle::get_charge(ReconstructedParticles)")
+               #selecting only final particles (status=1)
+               .Define("MC_final", "MCParticle::sel_genStatus(1)(Particle)")
 
-               #track parameters
-               .Define("RP_D0", "ReconstructedParticle2Track::geRP2TRKt_D0(ReconstructedParticles, EFlowTrack_1)")               
-               .Define("RP_Z0", "ReconstructedParticle2Track::geRP2TRKt_Z0(ReconstructedParticles, EFlowTrack_1)")               
-               .Define("RP_phi", "ReconstructedParticle2Track::geRP2TRKt_phi(ReconstructedParticles, EFlowTrack_1)")               
-               .Define("RP_omega", "ReconstructedParticle2Track::geRP2TRKt_omega(ReconstructedParticles, EFlowTrack_1)")               
-               .Define("RP_tanLambda", "ReconstructedParticle2Track::geRP2TRKt_tanLambda(ReconstructedParticles, EFlowTrack_1)")               
-               
-               #track parameters
-               .Define("RP_D0_sig", "ReconstructedParticle2Track::geRP2TRKt_D0_sig(ReconstructedParticles, EFlowTrack_1)")               
-               .Define("RP_Z0_sig", "ReconstructedParticle2Track::geRP2TRKt_Z0_sig(ReconstructedParticles, EFlowTrack_1)")               
+               .Define("MC_px",  "MCParticle::get_px(Particle)")
+               .Define("MC_py",  "MCParticle::get_py(Particle)")
+               .Define("MC_pz",  "MCParticle::get_pz(Particle)")
+               .Define("MC_p",  "MCParticle::get_p(Particle)")
+               .Define("MC_e",  "MCParticle::get_e(Particle)")
+               .Define("MC_theta",  "MCParticle::get_theta(Particle)")
+               .Define("MC_pdg", "MCParticle::get_pdg(Particle)")
+               .Define("MC_status", "MCParticle::get_genStatus(Particle)")
 
+               #momenta etc of final particles
+               .Define("MC_px_f",  "MCParticle::get_px(MC_final)")
+               .Define("MC_py_f",  "MCParticle::get_py(MC_final)")
+               .Define("MC_pz_f",  "MCParticle::get_pz(MC_final)")
+               .Define("MC_e_f",  "MCParticle::get_e(MC_final)")
+               .Define("MC_theta_f",  "MCParticle::get_theta(MC_final)")
+               .Define("MC_pdg_f", "MCParticle::get_pdg(MC_final)")
+
+               #get Ks->pi+-
+               .Define("K0spipi_indices", "MCParticle::get_indices_ExclusiveDecay(310, {211, -211}, true, true) (Particle, Particle1)")
+               .Define("pi0gammagamma_indices", "MCParticle::get_indices_ExclusiveDecay( 111, {22, 22}, true, true) ( Particle, Particle1)" )
+
+               #get parent indices
+               #.Define("MC_parent", "MCParticle::get_parentid(Particle0, Particle, Particle0)")
                
                #KT ALGORITHM
-               #build psedo-jets with the Reconstructed final particles
-               .Define("pseudo_jets", "JetClusteringUtils::set_pseudoJets(RP_px, RP_py, RP_pz, RP_e)")
+               #build psedo-jets with the MC final particles (status = 0)
+               .Define("pseudo_jets",        "JetClusteringUtils::set_pseudoJets(MC_px_f, MC_py_f, MC_pz_f, MC_e_f)")
 
-               #run jet clustering with all MC particles. kt_algorithm, R=0.5, exclusive clustering, exactly 4 jets, E-scheme
+               #run jet clustering with all MC particles. kt_algorithm, R=0.5, exclusive clustering, exactly 2 jets, E-scheme
                .Define("FCCAnalysesJets_kt", "JetClustering::clustering_kt(0.5, 2, 2, 1, 0)(pseudo_jets)")
 
                #get the jets out of the structure
-               .Define("jets_kt", "JetClusteringUtils::get_pseudoJets(FCCAnalysesJets_kt)")
+               .Define("jets_kt",            "JetClusteringUtils::get_pseudoJets(FCCAnalysesJets_kt)")
 
                #get the jet constituents out of the structure
                .Define("jetconstituents_kt", "JetClusteringUtils::get_constituents(FCCAnalysesJets_kt)")
 
                #get some jet variables
-               .Define("jets_kt_e",  "JetClusteringUtils::get_e(jets_kt)")
-               .Define("jets_kt_px", "JetClusteringUtils::get_px(jets_kt)")
-               .Define("jets_kt_py", "JetClusteringUtils::get_py(jets_kt)")
-               .Define("jets_kt_pz", "JetClusteringUtils::get_pz(jets_kt)")
+               .Define("jets_kt_e",          "JetClusteringUtils::get_e(jets_kt)")
+               .Define("jets_kt_px",         "JetClusteringUtils::get_px(jets_kt)")
+               .Define("jets_kt_py",         "JetClusteringUtils::get_py(jets_kt)")
+               .Define("jets_kt_pz",         "JetClusteringUtils::get_pz(jets_kt)")
+               .Define("jets_kt_flavour", "JetTaggingUtils::get_flavour(jets_kt, Particle)")
 
-               #get jet flavour
-               .Define("jets_kt_flavour", "JetTaggingUtils::get_flavour(jets_kt)")
 
                #EE-GENKT ALGORITHM
                #run jet clustering with all reconstructed particles. ee_genkt_algorithm, R=0.5, inclusive clustering, E-scheme 
-               .Define("FCCAnalysesJets_ee_genkt", "JetClustering::clustering_ee_genkt(0.5, 0, 0, 1, 0, 1)(pseudo_jets)")
+               .Define("FCCAnalysesJets_ee_genkt","JetClustering::clustering_ee_genkt(0.5, 0, 0, 1, 0, 1)(pseudo_jets)")
 
                #get the jets out of the struct
                .Define("jets_ee_genkt",           "JetClusteringUtils::get_pseudoJets(FCCAnalysesJets_ee_genkt)")
@@ -91,30 +97,26 @@ class analysis():
                .Define("jets_ee_genkt_px",        "JetClusteringUtils::get_px(jets_ee_genkt)")
                .Define("jets_ee_genkt_py",        "JetClusteringUtils::get_py(jets_ee_genkt)")
                .Define("jets_ee_genkt_pz",        "JetClusteringUtils::get_pz(jets_ee_genkt)")
+               .Define("jets_ee_genkt_flavour",   "JetTaggingUtils::get_flavour(jets_ee_genkt, Particle)")
 
-               #get jet flavour
-               .Define("jets_ee_genkt_flavour", "JetTaggingUtils::get_flavour(jets_ee_genkt)")
 
-               
                #EE-KT ALGORITHM
                #run jet clustering with all MC particles. ee_kt_algorithm, exclusive clustering, exactly 2 jets, E-scheme
                .Define("FCCAnalysesJets_ee_kt", "JetClustering::clustering_ee_kt(2, 2, 1, 0)(pseudo_jets)")
 
                #get the jets out of the structure
-               .Define("jets_ee_kt", "JetClusteringUtils::get_pseudoJets(FCCAnalysesJets_ee_kt)")
+               .Define("jets_ee_kt",            "JetClusteringUtils::get_pseudoJets(FCCAnalysesJets_ee_kt)")
 
                #get the jet constituents out of the structure
                .Define("jetconstituents_ee_kt", "JetClusteringUtils::get_constituents(FCCAnalysesJets_ee_kt)")
 
                #get some jet variables
-               .Define("jets_ee_kt_e",  "JetClusteringUtils::get_e(jets_ee_kt)")
-               .Define("jets_ee_kt_px", "JetClusteringUtils::get_px(jets_ee_kt)")
-               .Define("jets_ee_kt_py", "JetClusteringUtils::get_py(jets_ee_kt)")
-               .Define("jets_ee_kt_pz", "JetClusteringUtils::get_pz(jets_ee_kt)")
+               .Define("jets_ee_kt_e",          "JetClusteringUtils::get_e(jets_ee_kt)")
+               .Define("jets_ee_kt_px",         "JetClusteringUtils::get_px(jets_ee_kt)")
+               .Define("jets_ee_kt_py",         "JetClusteringUtils::get_py(jets_ee_kt)")
+               .Define("jets_ee_kt_pz",         "JetClusteringUtils::get_pz(jets_ee_kt)")
+               .Define("jets_ee_kt_flavour",    "JetTaggingUtils::get_flavour(jets_ee_kt, Particle)")
 
-               #get jet flavour
-               .Define("jets_ee_kt_flavour", "JetTaggingUtils::get_flavour(jets_ee_kt)")
-               
         )
 
         
@@ -123,24 +125,25 @@ class analysis():
         # select branches for output file
         branchList = ROOT.vector('string')()
         for branchName in [
-                "RP_px",
-                "RP_py",
-                "RP_pz",
-                "RP_p",
-                "RP_e",
-                "RP_theta",
-                "RP_mass",
-                "RP_charge",
+                "MC_px",
+                "MC_py",
+                "MC_pz",
+                "MC_p",
+                "MC_e",
+                "MC_theta",
+                "MC_pdg",
+                "MC_status",
 
-                "RP_D0",
-                "RP_Z0",
-                "RP_phi",
-                "RP_omega",
-                "RP_tanLambda",
+                "MC_px_f",
+                "MC_py_f",
+                "MC_pz_f",
+                "MC_e_f",
+                "MC_theta_f",
+                "MC_pdg_f",
 
-                "RP_D0_sig",
-                "RP_Z0_sig",
-                
+                "K0spipi_indices",
+                "pi0gammagamma_indices",
+
                 "jets_kt_e",
                 "jets_kt_px",
                 "jets_kt_py",
