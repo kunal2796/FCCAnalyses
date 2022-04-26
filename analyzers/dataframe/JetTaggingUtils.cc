1a8e6f572209acf153c8d9ebe4bcf65e5bd6bde7
Kunal Gautam <kgautam@lxplus724.cern.ch>
2022-04-26 19:11:27 +0200

    bug fixes

--- analyzers/dataframe/JetTaggingUtils.cc
+++ analyzers/dataframe/JetTaggingUtils.cc
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 0a2ec416e5f1e66d095b394dcf2d91a017cf5d46
 #include "JetTaggingUtils.h"
 using namespace JetTaggingUtils;
 
@@ -30,14 +26,14 @@ JetTaggingUtils::get_flavour(ROOT::VecOps::RVec<fastjet::PseudoJet> in,
       //if (deltaR <= 0.5 && gRandom->Uniform() <= efficiency) result[j] = true;
       
       Float_t dot = p.px() * parton.momentum.x
-                  + p.py() * parton.momentum.y
-                  + p.pz() * parton.momentum.z;
+	+ p.py() * parton.momentum.y
+	+ p.pz() * parton.momentum.z;
       Float_t lenSq1 = p.px() * p.px()
-                     + p.py() * p.py()
-                     + p.pz() * p.pz();
+	+ p.py() * p.py()
+	+ p.pz() * p.pz();
       Float_t lenSq2 = parton.momentum.x * parton.momentum.x
                      + parton.momentum.y * parton.momentum.y
-                     + parton.momentum.z * parton.momentum.z;
+	+ parton.momentum.z * parton.momentum.z;
       Float_t norm = sqrt(lenSq1*lenSq2);
       Float_t angle = acos(dot/norm);
 
@@ -54,9 +50,9 @@ JetTaggingUtils::get_flavour(ROOT::VecOps::RVec<fastjet::PseudoJet> in,
         } else {
           // if matched to quark, and this is a gluon, keep
           // previous result (favour quark)
-           ;
-        }   
-       }
+	  ;
+        }       
+      }
 
 
     }
@@ -142,41 +138,6 @@ JetTaggingUtils::sel_tag::operator()(ROOT::VecOps::RVec<bool> tags,
       if (!tags.at(i)) result.push_back(in.at(i));
     }
   }
-  return result;
-}
-
-ROOT::VecOps::RVec<int> JetTaggingUtils::get_flavour_qqbar(ROOT::VecOps::RVec<fastjet::PseudoJet> in, ROOT::VecOps::RVec<edm4hep::MCParticleData> MCin){
-  ROOT::VecOps::RVec<int> result(in.size(),0);
-
-  // ONLY FOR DIJETS FOR NOW
-  
-  int loopcount =0;
-  for (size_t i = 0; i < MCin.size(); ++i) {
-    auto & parton = MCin[i];
-    //Select partons only (for pythia 71-79):
-    if (parton.generatorStatus>80 || parton.generatorStatus<70) continue;
-    //Select outgoing partons from the hardest process - use ONLY(?) for Zqq events
-    //(For Zqq, ideally, don't even need anything else, just identify the partons)
-    //if (parton.generatorStatus!=23) continue;
-    if (parton.PDG > 5) continue;
-    ROOT::Math::PxPyPzMVector lv(parton.momentum.x, parton.momentum.y, parton.momentum.z, parton.mass);
-
-    for (size_t j = 0; j < in.size(); ++j) {
-      auto & p = in[j];
-      //float dEta = lv.Eta() - p.eta();
-      //float dPhi = lv.Phi() - p.phi();
-      //float deltaR = sqrt(dEta*dEta+dPhi*dPhi);
-      //if (deltaR <= 0.5 && gRandom->Uniform() <= efficiency) result[j] = true;
-      
-      Float_t dot = p.px()*parton.momentum.x+p.py()*parton.momentum.y+p.pz()*parton.momentum.z;
-      Float_t lenSq1 = p.px()*p.px()+p.py()*p.py()+p.pz()*p.pz();
-      Float_t lenSq2 = parton.momentum.x*parton.momentum.x+parton.momentum.y*parton.momentum.y+parton.momentum.z*parton.momentum.z;
-      Float_t norm = sqrt(lenSq1*lenSq2);
-      Float_t angle = acos(dot/norm);
-      if (angle <= 0.3) result[j] = parton.PDG; // unlikely that both q & qbar would be within the limit
-    }
-  }
-
   return result;
 }
 
@@ -380,150 +341,3 @@ std::vector<std::vector<int>> JetTaggingUtils::get_flavour_gm(ROOT::VecOps::RVec
   result.push_back(partonFlv);
   return result;
 }
-<<<<<<< HEAD
-=======
-#include "JetTaggingUtils.h"
-using namespace JetTaggingUtils;
-
-ROOT::VecOps::RVec<int>
-JetTaggingUtils::get_flavour(ROOT::VecOps::RVec<fastjet::PseudoJet> in,
-                             ROOT::VecOps::RVec<edm4hep::MCParticleData> MCin)
-{
-  ROOT::VecOps::RVec<int> result(in.size(),0);
-
-  int loopcount =0;
-  for (size_t i = 0; i < MCin.size(); ++i) {
-    auto & parton = MCin[i];
-    //Select partons only (for pythia8 71-79, for pythia6 2):
-    if ((parton.generatorStatus>80 ||
-         parton.generatorStatus<70) &&
-        parton.generatorStatus != 2 ) continue;
-    if (std::abs(parton.PDG) > 5 && parton.PDG!=21) continue;
-    ROOT::Math::PxPyPzMVector lv(parton.momentum.x, parton.momentum.y,
-                                 parton.momentum.z, parton.mass);
-
-    for (size_t j = 0; j < in.size(); ++j) {
-      auto & p = in[j];
-      //float dEta = lv.Eta() - p.eta();
-      //float dPhi = lv.Phi() - p.phi();
-      //float deltaR = sqrt(dEta*dEta+dPhi*dPhi);
-      //if (deltaR <= 0.5 && gRandom->Uniform() <= efficiency) result[j] = true;
-      
-      Float_t dot = p.px() * parton.momentum.x
-                  + p.py() * parton.momentum.y
-                  + p.pz() * parton.momentum.z;
-      Float_t lenSq1 = p.px() * p.px()
-                     + p.py() * p.py()
-                     + p.pz() * p.pz();
-      Float_t lenSq2 = parton.momentum.x * parton.momentum.x
-                     + parton.momentum.y * parton.momentum.y
-                     + parton.momentum.z * parton.momentum.z;
-      Float_t norm = sqrt(lenSq1*lenSq2);
-      Float_t angle = acos(dot/norm);
-
-      if (angle <= 0.3) {
-        if (result[j]==21 or result[j]==0) {
-          // if no match before, or matched to gluon, match to
-          // this particle (favour quarks over gluons)
-          result[j] = std::abs ( parton.PDG );
-        }
-        else if (parton.PDG!=21) {
-          // if matched to quark, and this is a quark, favour
-          // heavier flavours
-          result[j] = std::max(result[j], std::abs ( parton.PDG ));
-        } else {
-          // if matched to quark, and this is a gluon, keep
-          // previous result (favour quark)
-           ;
-        }       
-       }
-
-
-    }
-  }
-
-  return result;
-}
-
-ROOT::VecOps::RVec<int>
-JetTaggingUtils::get_btag(ROOT::VecOps::RVec<int> in,
-                          float efficiency, float mistag_c,
-                          float mistag_l, float mistag_g) {
-
-  ROOT::VecOps::RVec<int> result(in.size(),0);
-
-  for (size_t j = 0; j < in.size(); ++j) {
-    if (in.at(j) ==  5 && gRandom->Uniform() <= efficiency) result[j] = 1;
-    if (in.at(j) ==  4 && gRandom->Uniform() <= mistag_c) result[j] = 1;
-    if (in.at(j)  <  4 && gRandom->Uniform() <= mistag_l) result[j] = 1;
-    if (in.at(j) == 21 && gRandom->Uniform() <= mistag_g) result[j] = 1;
-  }
-  return result;
-}
-
-ROOT::VecOps::RVec<int>
-JetTaggingUtils::get_ctag(ROOT::VecOps::RVec<int> in,
-                          float efficiency, float mistag_b,
-                          float mistag_l, float mistag_g) {
-
-  ROOT::VecOps::RVec<int> result(in.size(),0);
-
-  for (size_t j = 0; j < in.size(); ++j) {
-    if (in.at(j) ==  4 && gRandom->Uniform() <= efficiency) result[j] = 1;
-    if (in.at(j) ==  5 && gRandom->Uniform() <= mistag_b) result[j] = 1;
-    if (in.at(j)  <  4 && gRandom->Uniform() <= mistag_l) result[j] = 1;
-    if (in.at(j) == 21 && gRandom->Uniform() <= mistag_g) result[j] = 1;
-  }
-  return result;
-}
-
-ROOT::VecOps::RVec<int>
-JetTaggingUtils::get_ltag(ROOT::VecOps::RVec<int> in,
-                          float efficiency, float mistag_b,
-                          float mistag_c, float mistag_g) {
-
-  ROOT::VecOps::RVec<int> result(in.size(),0);
-
-  for (size_t j = 0; j < in.size(); ++j) {
-    if (in.at(j) <  4  && gRandom->Uniform() <= efficiency) result[j] = 1;
-    if (in.at(j) ==  5 && gRandom->Uniform() <= mistag_b) result[j] = 1;
-    if (in.at(j) ==  4 && gRandom->Uniform() <= mistag_c) result[j] = 1;
-    if (in.at(j) == 21 && gRandom->Uniform() <= mistag_g) result[j] = 1;
-  }
-  return result;
-}
-
-ROOT::VecOps::RVec<int>
-JetTaggingUtils::get_gtag(ROOT::VecOps::RVec<int> in,
-                          float efficiency, float mistag_b,
-                          float mistag_c, float mistag_l) {
-
-  ROOT::VecOps::RVec<int> result(in.size(),0);
-
-  for (size_t j = 0; j < in.size(); ++j) {
-    if (in.at(j) == 21 && gRandom->Uniform() <= efficiency) result[j] = 1;
-    if (in.at(j) ==  5 && gRandom->Uniform() <= mistag_b) result[j] = 1;
-    if (in.at(j) ==  4 && gRandom->Uniform() <= mistag_c) result[j] = 1;
-    if (in.at(j)  <  4 && gRandom->Uniform() <= mistag_l) result[j] = 1;
-  }
-  return result;
-}
-
-JetTaggingUtils::sel_tag::sel_tag(bool arg_pass): m_pass(arg_pass) {};
-ROOT::VecOps::RVec<fastjet::PseudoJet>
-JetTaggingUtils::sel_tag::operator()(ROOT::VecOps::RVec<bool> tags,
-                                     ROOT::VecOps::RVec<fastjet::PseudoJet> in){
-  ROOT::VecOps::RVec<fastjet::PseudoJet> result;
-  for (size_t i = 0; i < in.size(); ++i) {
-    if (m_pass) {
-      if (tags.at(i)) result.push_back(in.at(i));
-    }
-    else {
-      if (!tags.at(i)) result.push_back(in.at(i));
-    }
-  }
-  return result;
-}
->>>>>>> 23bc7613c91d271a0f3c5065bed3c6fd4fb7d930
-=======
->>>>>>> 0a2ec416e5f1e66d095b394dcf2d91a017cf5d46
--- analyzers/dataframe/JetTaggingUtils.h
+++ analyzers/dataframe/JetTaggingUtils.h
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 0a2ec416e5f1e66d095b394dcf2d91a017cf5d46
 #ifndef  JETTAGGINGUTILS_ANALYZERS_H
 #define  JETTAGGINGUTILS_ANALYZERS_H
 
@@ -11,7 +7,6 @@
 #include "edm4hep/MCParticleData.h"
 #include "fastjet/JetDefinition.hh"
 #include "TRandom3.h"
-#include "TLorentzVector.h"
 
 /** Jet tagging utilities interface.
 This represents a set functions and utilities to perfom jet tagging from a list of jets.
@@ -21,7 +16,7 @@ namespace JetTaggingUtils{
 
   /** @name JetTaggingUtils
    *  Jet tagging interface utilities.
-  */
+   */
 
   //Get flavour association of jet 
   ROOT::VecOps::RVec<int> get_flavour(ROOT::VecOps::RVec<fastjet::PseudoJet> in, ROOT::VecOps::RVec<edm4hep::MCParticleData> MCin);
@@ -40,8 +35,6 @@ namespace JetTaggingUtils{
     sel_tag(bool arg_pass);
     ROOT::VecOps::RVec<fastjet::PseudoJet> operator() (ROOT::VecOps::RVec<bool> tags, ROOT::VecOps::RVec<fastjet::PseudoJet> in);
   };
-  //Get flavour association of jet 
-  ROOT::VecOps::RVec<int> get_flavour_qqbar(ROOT::VecOps::RVec<fastjet::PseudoJet> in, ROOT::VecOps::RVec<edm4hep::MCParticleData> MCin);
 
   //Get flavour association of jet via ghost matching (manual)
   ROOT::VecOps::RVec<int> get_flavour_gm_manual(ROOT::VecOps::RVec<fastjet::PseudoJet> in, std::vector<std::vector<int>> inJC, ROOT::VecOps::RVec<float> pdg_gm);
@@ -51,7 +44,7 @@ namespace JetTaggingUtils{
   ROOT::VecOps::RVec<int> get_flavour_gm7x_auto(ROOT::VecOps::RVec<fastjet::PseudoJet> in, std::vector<std::vector<int>> inJC, ROOT::VecOps::RVec<edm4hep::MCParticleData> MCin, std::vector<fastjet::PseudoJet> PJin);
   //Get flavour association of jet via ghost matching (automated status 71-79, with cut on |p|)
   ROOT::VecOps::RVec<int> get_flavour_gm_pcut(ROOT::VecOps::RVec<fastjet::PseudoJet> in, std::vector<std::vector<int>> inJC, ROOT::VecOps::RVec<edm4hep::MCParticleData> MCin, std::vector<fastjet::PseudoJet> PJin, float p_cut);
-
+  
   //Get flavour association of jet via ghost matching with cut on |p|
   std::vector<std::vector<int>> get_flavour_gm(ROOT::VecOps::RVec<fastjet::PseudoJet> in, std::vector<std::vector<int>> inJC, ROOT::VecOps::RVec<edm4hep::MCParticleData> MCin, std::vector<fastjet::PseudoJet> PJin, int statCode, float p_cut);
   // in: jets that need to be assigned a flavour
@@ -60,56 +53,9 @@ namespace JetTaggingUtils{
   // PJin: jet constituents before clustering (excluding ghosts)
   // statCode: 0->23, 1->71-79 (choice of parton status codes)
   // p_cut: choose 0 for no cut
-  
-    ///@}                                                                                                                                                                              
-}
 
-
-#endif
-<<<<<<< HEAD
-=======
-#ifndef  JETTAGGINGUTILS_ANALYZERS_H
-#define  JETTAGGINGUTILS_ANALYZERS_H
-
-#include <vector>
-#include "Math/Vector4D.h"
-#include "ROOT/RVec.hxx"
-#include "edm4hep/MCParticleData.h"
-#include "fastjet/JetDefinition.hh"
-#include "TRandom3.h"
-
-/** Jet tagging utilities interface.
-This represents a set functions and utilities to perfom jet tagging from a list of jets.
-*/
-
-namespace JetTaggingUtils{
-
-  /** @name JetTaggingUtils
-   *  Jet tagging interface utilities.
-  */
-
-  //Get flavour association of jet 
-  ROOT::VecOps::RVec<int> get_flavour(ROOT::VecOps::RVec<fastjet::PseudoJet> in, ROOT::VecOps::RVec<edm4hep::MCParticleData> MCin);
-  //Get b-tags with an efficiency applied
-  ROOT::VecOps::RVec<int> get_btag(ROOT::VecOps::RVec<int> in, float efficiency, float mistag_c=0., float mistag_l=0., float mistag_g=0.);
-  //Get c-tags with an efficiency applied
-  ROOT::VecOps::RVec<int> get_ctag(ROOT::VecOps::RVec<int> in, float efficiency, float mistag_b=0., float mistag_l=0., float mistag_g=0.);
-  //Get l-tags with an efficiency applied
-  ROOT::VecOps::RVec<int> get_ltag(ROOT::VecOps::RVec<int> in, float efficiency, float mistag_b=0., float mistag_c=0., float mistag_g=0.);
-  //Get g-tags with an efficiency applied
-  ROOT::VecOps::RVec<int> get_gtag(ROOT::VecOps::RVec<int> in, float efficiency, float mistag_b=0., float mistag_c=0., float mistag_l=0.);
-
-  /// select a list of jets depending on the status of a certain boolean flag (corresponding to its tagging state)
-  struct sel_tag {
-    bool m_pass; // if pass is true, select tagged jets. Otherwise select anti-tagged ones
-    sel_tag(bool arg_pass);
-    ROOT::VecOps::RVec<fastjet::PseudoJet> operator() (ROOT::VecOps::RVec<bool> tags, ROOT::VecOps::RVec<fastjet::PseudoJet> in);
-  };
 
   ///@}
 }
 
 #endif
->>>>>>> 23bc7613c91d271a0f3c5065bed3c6fd4fb7d930
-=======
->>>>>>> 0a2ec416e5f1e66d095b394dcf2d91a017cf5d46
