188339d3024f82675e18a1797777740ef3982967
Kunal Gautam <kunal2796@gmail.com>
2022-05-06 12:22:01 +0200

    fix

--- analyzers/dataframe/JetClusteringUtils.cc
+++ analyzers/dataframe/JetClusteringUtils.cc
@@ -184,7 +184,7 @@ ROOT::VecOps::RVec<float> JetClusteringUtils::get_e(ROOT::VecOps::RVec<fastjet::
 ROOT::VecOps::RVec<float> JetClusteringUtils::get_p(ROOT::VecOps::RVec<fastjet::PseudoJet> in){
   ROOT::VecOps::RVec<float> result;
   for (auto & p: in) {
-    float mom = sqrt(p.px*p.px + p.py*p.py + p.pz*p.pz);
+    float mom = sqrt(p.px()*p.px() + p.py()*p.py() + p.pz()*p.pz());
     result.push_back(mom);
   }
   return result;
