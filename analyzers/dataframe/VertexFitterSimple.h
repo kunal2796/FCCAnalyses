ea34b8e26f5bb0a4e7e7ddc497ac6551499a2418
Kunal Gautam <kunal2796@gmail.com>
2022-05-06 12:32:04 +0200

    fix

--- analyzers/dataframe/VertexFitterSimple.h
+++ analyzers/dataframe/VertexFitterSimple.h
@@ -162,17 +162,17 @@ namespace VertexFitterSimple{
   VertexingUtils::FCCAnalysesV0 get_V0s( ROOT::VecOps::RVec<edm4hep::TrackState> np_tracks,
 					 VertexingUtils::FCCAnalysesVertex PV,
 					 bool tight = true,
-					 double chi2_cut=9.) ;
+					 double chi2_cut=9. ) ;
 
-  VertexingUtils::FCCAnalysesV0 get_V0s_jet(ROOT::VecOps::RVec<edm4hep::ReconstructedParticleData> recoparticles,
-					    ROOT::VecOps::RVec<edm4hep::TrackState> thetracks,
-					    ROOT::VecOps::RVec<bool> isInPrimary,
-					    ROOT::VecOps::RVec<fastjet::PseudoJet> jets,
-					    std::vector<std::vector<int>> jet_consti,
-					    VertexingUtils::FCCAnalysesVertex PV,
-					    bool tight = true,
-					    double chi2_cut=9.)
-
+  VertexingUtils::FCCAnalysesV0 get_V0s_jet( ROOT::VecOps::RVec<edm4hep::ReconstructedParticleData> recoparticles,
+					     ROOT::VecOps::RVec<edm4hep::TrackState> thetracks,
+					     ROOT::VecOps::RVec<bool> isInPrimary,
+					     ROOT::VecOps::RVec<fastjet::PseudoJet> jets,
+					     std::vector<std::vector<int>> jet_consti,
+					     VertexingUtils::FCCAnalysesVertex PV,
+					     bool tight = true,
+					     double chi2_cut=9. );
+  
 
   Double_t FastRv(TVectorD p1, TVectorD p2) ;
   TMatrixDSym RegInv3(TMatrixDSym &Smat0) ;
