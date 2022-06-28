f82cdb310ce77c3128393630f2f63dbc29fd2809
github-action-benchmark <github@users.noreply.github.com>
2022-06-28 15:59:21 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 0399a7f15f66729f39e21ee1798a7599616a274c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1656431959884,
+  "lastUpdate": 1656431961350,
   "repoUrl": "https://github.com/kunal2796/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -470,6 +470,40 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/flavour/Bc2TauNu"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "kunal2796@gmail.com",
+            "name": "Kunal Gautam",
+            "username": "kunal2796"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0399a7f15f66729f39e21ee1798a7599616a274c",
+          "message": "Merge pull request #95 from kunal2796/ROOT\n\nadd some ntuplers",
+          "timestamp": "2022-06-28T17:51:06+02:00",
+          "tree_id": "6954e6f37376dcbcd64d7e60231bc3ff96fe2289",
+          "url": "https://github.com/kunal2796/FCCAnalyses/commit/0399a7f15f66729f39e21ee1798a7599616a274c"
+        },
+        "date": 1656431960914,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu",
+            "value": 7.551057806899312,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/flavour/Bc2TauNu",
+            "value": 6.189168509473368,
+            "unit": "Evt/s"
           }
         ]
       }
