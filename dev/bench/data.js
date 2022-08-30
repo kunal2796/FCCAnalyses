64e4638ed77cc9b68128b084450f1943fcbc3659
github-action-benchmark <github@users.noreply.github.com>
2022-08-30 13:19:54 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for ee09b5d146773964571aff1e57044d1da167c94a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1661865482710,
+  "lastUpdate": 1661865594721,
   "repoUrl": "https://github.com/kunal2796/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -2940,6 +2940,51 @@ window.BENCHMARK_DATA = {
             "name": "nightly | Events processed per second: examples/FCCee/test",
             "value": 8.676766001954716,
             "unit": "Evt/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "kunal2796@gmail.com",
+            "name": "Kunal Gautam",
+            "username": "kunal2796"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ee09b5d146773964571aff1e57044d1da167c94a",
+          "message": "Merge pull request #100 from kunal2796/ROOT\n\nfn updates for SV in jets",
+          "timestamp": "2022-08-30T15:09:32+02:00",
+          "tree_id": "0ee4495e67262195999c328649ba7fde588e8384",
+          "url": "https://github.com/kunal2796/FCCAnalyses/commit/ee09b5d146773964571aff1e57044d1da167c94a"
+        },
+        "date": 1661865593270,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/higgs/mH-recoil/mumu",
+            "value": 12.640312671661377,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/higgs/mH-recoil/mumu"
+          },
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/flavour/Bc2TauNu",
+            "value": 17.004034280776978,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/flavour/Bc2TauNu"
+          },
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/test",
+            "value": 12.661748170852661,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/test"
           }
         ]
       }
