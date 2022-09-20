2f6e5f24c9508ff44505ce7265b0daa1f0283128
github-action-benchmark <github@users.noreply.github.com>
2022-09-20 07:48:08 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 8146ffb8b2c015e2eb1f020263b5c1e0ef2a0411

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1663660087048,
+  "lastUpdate": 1663660088521,
   "repoUrl": "https://github.com/kunal2796/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -3387,6 +3387,43 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/test"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "kgautam@lxplus724.cern.ch",
+            "name": "Kunal Gautam"
+          },
+          "committer": {
+            "email": "kgautam@lxplus724.cern.ch",
+            "name": "Kunal Gautam"
+          },
+          "distinct": true,
+          "id": "8146ffb8b2c015e2eb1f020263b5c1e0ef2a0411",
+          "message": "several code updates for the ntupler",
+          "timestamp": "2022-09-20T09:39:03+02:00",
+          "tree_id": "c0375a8569e33eff3fe78f0179fd70334908cba0",
+          "url": "https://github.com/kunal2796/FCCAnalyses/commit/8146ffb8b2c015e2eb1f020263b5c1e0ef2a0411"
+        },
+        "date": 1663660088072,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu",
+            "value": 8.937740258942132,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/flavour/Bc2TauNu",
+            "value": 5.934264042593439,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/test",
+            "value": 8.43456422927707,
+            "unit": "Evt/s"
           }
         ]
       }
