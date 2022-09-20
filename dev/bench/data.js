aa2f8faf761651a02833378904b34f028ad4063c
github-action-benchmark <github@users.noreply.github.com>
2022-09-20 10:25:11 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 83cc6b9c27e23a69dbe58344075e0bed9906fd7d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1663669508031,
+  "lastUpdate": 1663669511232,
   "repoUrl": "https://github.com/kunal2796/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -3633,6 +3633,45 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/test"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "kunal2796@gmail.com",
+            "name": "Kunal Gautam",
+            "username": "kunal2796"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "83cc6b9c27e23a69dbe58344075e0bed9906fd7d",
+          "message": "Merge pull request #103 from HEP-FCC/master\n\nmerging central repo",
+          "timestamp": "2022-09-20T12:10:26+02:00",
+          "tree_id": "2f779bc7550dfa3469cb3a0396e78c91dac25c87",
+          "url": "https://github.com/kunal2796/FCCAnalyses/commit/83cc6b9c27e23a69dbe58344075e0bed9906fd7d"
+        },
+        "date": 1663669509700,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu",
+            "value": 6.386080632203566,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/flavour/Bc2TauNu",
+            "value": 4.180621578086806,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/test",
+            "value": 5.790267577669002,
+            "unit": "Evt/s"
           }
         ]
       }
